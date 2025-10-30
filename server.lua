63d1832f9e79145952ef44f740e3529e2be888a9
Somis12 <126623439+Somis12@users.noreply.github.com>
2025-10-30 23:49:41 +0200

    Add files via upload

--- server.lua
+++ server.lua
@@ -1,20 +1,54 @@
 
 local veh_plates = {}
 
-AddEventHandler('somis-betterevents:vehicleEntered', function(source, vehicle)
-    local plate = GetVehicleNumberPlateText(vehicle)
-    veh_plates[source] = "glove" .. plate
+AddEventHandler('entityCreated', function(entity)
+  if not DoesEntityExist(entity) or GetEntityType(entity) ~= 2 then
+        return
+    end
+    SetTimeout(2000, function()
+    local plate = GetVehicleNumberPlateText(entity)
+    if plate then
+        local netId = NetworkGetNetworkIdFromEntity(entity)
+        veh_plates[netId] = plate
+    end
+    end)
 end)
 
-AddEventHandler('somis-betterevents:vehicleExit', function(source, vehicle)
-    veh_plates[source] = nil
+AddEventHandler('serverEntityCreated', function(entity)
+    if not DoesEntityExist(entity) or GetEntityType(entity) ~= 2 then
+        return
+    end
+    SetTimeout(2000, function()
+    local plate = GetVehicleNumberPlateText(entity)
+    if plate then
+        local netId = NetworkGetNetworkIdFromEntity(entity)
+        veh_plates[netId] = plate
+    end
+    end)
+end)
+
+
+AddEventHandler('entityRemoved', function(entity)
+    local netId = NetworkGetNetworkIdFromEntity(entity)
+    if netId and veh_plates[netId] then
+        veh_plates[netId] = nil
+    end
 end)
 
 local hookId = exports.ox_inventory:registerHook('openInventory', function(data)
     if data.inventoryType == 'glovebox' then
-        local plate = veh_plates[data.source]
-        if not plate then return true end
-        if tostring(plate) ~= tostring(data.inventoryId) then
+        local plate = veh_plates[data.netId]
+        if not plate then return false end
+        local cleanId = tostring(data.inventoryId):gsub("^glove", "")
+        if tostring(plate) ~= cleanId then
+            return false
+        end
+    end
+    if data.inventoryType == 'trunk' then
+        local plate = veh_plates[data.netId]
+        if not plate then return false end
+        local cleanId = tostring(data.inventoryId):gsub("^trunk", "")
+        if tostring(plate) ~= cleanId then
             return false
         end
     end
